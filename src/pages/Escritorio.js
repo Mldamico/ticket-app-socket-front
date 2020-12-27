592d6e06b9771d415e99375307e0a949e6d0472f
mldamico <matias.lioneldamico@gmail.com>
2020-12-27 16:18:18 -0300

    Get usuario from storage

--- src/pages/Escritorio.js
+++ src/pages/Escritorio.js
@@ -1,21 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { Row, Col, Typography, Button, Divider } from "antd";
 import { CloseCircleOutlined, RightOutlined } from "@ant-design/icons";
 import { useHideMenu } from "../hooks/useHideMenu";
+import { getUsuarioStorage } from "../helpers/getUsuarioStorage";
+import { Redirect, useHistory } from "react-router-dom";
 
 const { Title, Text } = Typography;
 
 export const Escritorio = () => {
+  const [usuario] = useState(getUsuarioStorage());
   useHideMenu(false);
-  const salir = () => {};
+  const history = useHistory();
+  const salir = () => {
+    localStorage.clear();
+    history.replace("/ingresar");
+  };
   const siguienteTicket = () => {};
+  if (!usuario.agente || !usuario.escritorio) {
+    return <Redirect to="/ingresar" />;
+  }
   return (
     <>
       <Row>
         <Col span={20}>
-          <Title level={2}>Fernando</Title>
+          <Title level={2}>{usuario.agente}</Title>
           <Text>Usted esta trabajando en el escritorio</Text>
-          <Text type="success">5</Text>
+          <Text type="success">{usuario.escritorio}</Text>
         </Col>
         <Col span={4} align="right">
           <Button shape="round" type="danger" onClick={salir}>
